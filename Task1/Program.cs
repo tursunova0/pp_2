ddc7e8a138bbb9e04eb0b22a7d0a389d6581b236
tursunova00 <aigerimtursunova@mail.ru>
2019-02-09 10:34:55 +0600

    text

---
+++ Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static public bool Prime(int n)
+        {
+            if ((n == 0) || (n == 1)) // 1 и 0 не являются простыми числами
+                return false;
+            else
+            {
+                for (int i = 2; i <= Math.Sqrt(n); i++)
+                {
+                    if (n % i == 0) // так же число не является простым, если оно имеет больше двух делителей, т.е. не только 1 и само число
+                        return false;
+                }
+            }
+            return true; // если все условия соблюдены, число яляется простым
+        }
+        static void Main(string[] args)
+
+        {
+            int p = int.Parse(Console.ReadLine());  // считываю количество элементов
+            string s = Console.ReadLine(); // считываю строку со значениями элементов
+            string[] ss = s.Split(); // разделяю элементы строки
+            int[] a = new int[p]; // создаю пустой массив чисел
+            for (int i = 0; i < p; i++)
+            {
+                a[i] = int.Parse(ss[i]); // присваиваю элементу массива соответствующий элемент строки
+            }
+            int cnt = 0; // создаю новое значение
+            for (int i = 0; i < p; i++)
+            {
+                if (Prime(a[i]) == true)
+                    cnt++; // определяю количество нужных элементов (простых чисел)
+            }
+            Console.WriteLine(cnt); // вывожу количество простых чисел
+            for (int i = 0; i < p; i++)
+            {
+                if (Prime(a[i]) == true)
+                    Console.Write(a[i] + " "); // вывожу сами числа
+            }
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Student // создаю класс
+    {
+        private string name;  // создаю его переменные
+        private string id;
+        private int year;
+
+        public Student(string name, string id) // создаю конструктор
+        {
+            this.name = name;
+            this.id = id;
+            this.year = 1;
+        }
+
+        public void PrintInfo() // вывожу информацию о студенте
+        {
+            Console.WriteLine(name + " " + id + " " + year);
+        }
+
+        public void Incr()  // увеличаю год обучения
+        {
+            year++;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("Dias", "18BD"); // создаю нового студента
+            s.PrintInfo(); // вывожу начальные данные
+            s.Incr(); // увеличиваю год обучения
+            s.PrintInfo(); // вывожу конечные данные
+        }
+    }
+}
